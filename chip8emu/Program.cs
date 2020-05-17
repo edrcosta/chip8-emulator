4ef6d656d6f39258379b00ef36d4003370cff7fb
Eder Costa <eder.costa@laborit.com.br>
2020-05-17 15:09:39 -0300

    init a projet

---
+++ chip8emu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace chip8emu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
