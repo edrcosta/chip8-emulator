d067d399175bfcdc3cdb18008a9d4659f01337b3
Eder Costa <eder.costa@laborit.com.br>
2020-05-17 15:31:48 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# CHIP 8 Emulator
+
+This is an implementation of https://en.wikipedia.org/wiki/CHIP-8 
+
+the ideia is run games for CHIP8. 
+
+
+## Requirements
+
+* [dot net core](https://dotnet.microsoft.com/download)
+
+## Usage
+
+1. Navegate to the folder 
+2. Run `dotnet run`
+
+## Contributing
+
+Please contribute using [Github Flow](https://guides.github.com/introduction/flow/). Create a branch, add commits and write a [pull request](https://help.github.com/en/github/collaborating-with-issues-and-pull-requests/about-pull-requests).
--- chip8emu/Program.cs
+++ chip8emu/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace chip8emu
 {
@@ -6,7 +8,36 @@ namespace chip8emu
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string fileName = @"roms/BLITZ";
+            FileStream readStream;
+            readStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader readBinary = new BinaryReader(readStream);
+
+            byte inbyte;
+            string outbyte;
+            int value;
+
+            // inbyte = readBinary.ReadByte(); // read in the value: 7F in hex , 127 in decimal
+            // value = inbyte << 8; // shift the value 8 bits to the left: 7F 00 in hex, 32512 in decimal
+            // inbyte = readBinary.ReadByte(); // read in the next value: CB in hex, 203 in decimal
+            // value += inbyte; // add the second byte to the first: 7F CB in hex, 32715 in decimal
+            Console.WriteLine(ReadFully(readStream)); // writes 32715 to the console
+        }
+
+
+        public static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16*1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
         }
     }
 }
